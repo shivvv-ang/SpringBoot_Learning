338f4a1fa952a428de5ba16bfbe88140aab40a43
shivvv-ang <shivangramakanthariakar@gmail.com>
2026-08-05 22:03:26 +0530

    feat(spring): explore Jackson data binding, @PathVariable and bean lifecycle

--- 04-springBoot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentErrorResponse.java
+++ 04-springBoot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentErrorResponse.java
@@ -1,4 +1,14 @@
 package com.example.demo.rest;
 
 public class StudentErrorResponse {
+
+    private String message;
+
+    private int status;
+
+    private long timeStamp;
+
+    public StudentErrorResponse(){
+
+    }
 }
--- 04-springBoot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-springBoot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,4 +1,42 @@
 package com.example.demo.rest;
 
+import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+
 public class StudentRestController {
+
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+
+        students.add(new Student("atharva","khedukar"));
+        students.add(new Student("sumit","kissan"));
+        students.add(new Student("Arin","b"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents()
+    {
+        return students;
+    }
+
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable("studentId") int studentId)
+    {
+        return students.get(studentId);
+    }
+
 }
